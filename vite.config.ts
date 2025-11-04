6c341ef283d26300fac7a75e29efff6a4248fab3
Jimmy <jimmymiguelgalarza@gmail.com>
2025-11-03 20:57:47 -0500

    resolviendo ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/Game-Piedra-Papel-Tijera/',
+  base: "/Game-Piedra-Papel-Tijera/",
   build: {
-    outDir: 'dist', // carpeta de salida
-    rollupOptions: {
-      input: 'src/main.ts', // archivo principal de tu proyecto, ajustalo si usás otro
-    },
+    outDir: "dist",
   },
-})
+});
